bb893ab3ba3e372f59ad4c435bbe221faaf63113
Lyudmila Dermenzhii <lsmolyaninova@gmail.com>
2022-06-13 18:52:13 +0300

    add files of previous homework

---
+++ README.pl.md
@@ -0,0 +1,1 @@
+https://LyudmilaD.github.io/goit-react-hw-02-feedback/
---
+++ src/components/App.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import Statistics from './feedback/statistics';
+import Section from './feedback/section';
+import FeedbackOptions from './feedback/feedbackOptions';
+import Notification from './feedback/notification';
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((total, item) => total + item, 0);
+  };
+
+  countPositiveFeedbackPercentage = () => {
+    return ((this.state.good / this.countTotalFeedback()) * 100).toFixed(2);
+  };
+
+  onLeaveFeedback = e => {
+    const name = e.target.name;
+    this.setState(prevState => ({
+      [name]: prevState[name] + 1,
+    }));
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage();
+    const objKey = Object.keys(this.state);
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={objKey}
+            onLeaveFeedback={this.onLeaveFeedback}
+          />
+        </Section>
+
+        {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={positivePercentage}
+            />
+          </Section>
+        )}
+      </>
+    );
+  }
+}
